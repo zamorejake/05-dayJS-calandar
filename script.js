baff92b0cb990265cca8ded1828be83984999857
Jake <zamorejake@gmail.com>
2023-04-20 20:56:06 -0500

    finished up css selectors to change colors, finalized code and removed unneeded comments, used prettier to make it look nice

--- script.js
+++ script.js
@@ -1,14 +1,6 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
+  // TODO: Add a listener for click events on the save button.
+
   let button = document.getElementsByClassName("btn saveBtn col-2 col-md-1");
 
   for (var i = 0; i < button.length; i++) {
@@ -18,7 +10,6 @@ $(function () {
   function saveContent(e) {
     let text = this.previousElementSibling;
     let parent = e.target.parentElement.id;
-    let combo = [parent, text.value];
     let currentCalendar = JSON.parse(localStorage.getItem("schedule"));
 
     if (currentCalendar) {
@@ -48,35 +39,25 @@ $(function () {
   }
 
   // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  //<div id="hour-9" class="row time-block past">
-  //document.getElementById("MyElement").classList.add('MyClass');
-  var currentHour = dayjs().hour()
-  var timeID = document.querySelectorAll("#hour-9, #hour-10, #hour-11, #hour-12, #hour-13, #hour-14, #hour-15, #hour-16, #hour-17");
-  if (currentHour < 9) {
-    for (var i = 0; i < currentHour; i++) {
-      timeID[i].classList.add("future");
-    }
-  }
-  if (currentHour = 9) {
-    for (var i = 0; i < currentHour; i++) {
-      timeID[0].classList.add("present");
-    }
-  }
-  if (currentHour > 17) {
-    for (var i = 0; i < currentHour; i++) {
-      timeID[i].classList.add("past");
+
+  let currentHour = dayjs().hour();
+  console.log(currentHour);
+  let fields = document.getElementsByClassName("time-block");
+
+  for (let i = 0; i < fields.length; i++) {
+    let timeID = fields[i].id;
+    let hourID = parseInt(timeID.split("-")[1]);
+
+    if (hourID < currentHour) {
+      fields[i].classList.add("past");
+    } else if (hourID === currentHour) {
+      fields[i].classList.add("present");
+    } else {
+      fields[i].classList.add("future");
     }
   }
 
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
+  // TODO: Add code to get any user input that was saved in localStorage
 
   let textDesc = document.getElementsByClassName("col-8 col-md-10 description");
 
